80639145b307d69601ecc880ed1520617667c535
Usserski <114247414+Usserski@users.noreply.github.com>
2023-01-31 11:16:35 +0100

    Update zaczynamy.py

--- zaczynamy.py
+++ zaczynamy.py
@@ -1,14 +1,25 @@
-class Person:
-    def __init__(self, name, sex):
-        self.name = name
-        self.sex = sex
+# zmienne ( Variables)
+# price = 10
+# print(price)
 
-    def talk(self):
-        print(f"Hi, I am {self.name}")
-        print(f" I am {self.sex}")
+# zmiana typu zmiennych ( change variables type )
+# waga_w_kg = input('Waga w (kg): ')
+# waga_w_lbs = int(waga_w_kg) * 2
+# print(f'Waga_w_lbs: ', waga_w_lbs)
 
+# korzystanie z roznych edycji tekstow(using a diffrent ways to edit text)
+# kurs = "python dla poczatkujacych"
+# kurs1 = 'python dla "poczatkujacych"'
+# print(kurs)
+# print(kurs1)
+# print(kurs[0:])
+# print(kurs[:])
+# print(kurs[0:5])
 
-katrin = Person("Kathrin Smith", "Female")
-katrin.talk()
-john = Person("John Smith", "Male")
-john.talk()
+# formatowanie stringow ( formating strings)
+#imie = 'Jan'
+#nazwisko = 'Kowalski'
+#wiadomosc = imie + ' [' + nazwisko + '] ' + 'jest programista'
+#wiado = f'{imie} [{nazwisko}] jest programista'
+# print(wiadomosc)
+# print(wiado)
