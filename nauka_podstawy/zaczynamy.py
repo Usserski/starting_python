cb97b68b8608508de0a947b9e1d25bec2bbc1b74
Usserski <114247414+Usserski@users.noreply.github.com>
2023-02-06 19:04:19 +0100

    Create zaczynamy.py

---
+++ nauka_podstawy/zaczynamy.py
@@ -0,0 +1,51 @@
+# zmienne ( Variables)
+# price = 10
+# print(price)
+
+# zmiana typu zmiennych ( change variables type )
+# waga_w_kg = input('Waga w (kg): ')
+# waga_w_lbs = int(waga_w_kg) * 2
+# print(f'Waga_w_lbs: ', waga_w_lbs)
+
+# korzystanie z roznych edycji tekstow(using a diffrent ways to edit text)
+# kurs = "python dla poczatkujacych"
+# kurs1 = 'python dla "poczatkujacych"'
+# print(kurs)
+# print(kurs1)
+# print(kurs[0:])
+# print(kurs[:])
+# print(kurs[0:5])
+
+# formatowanie stringow ( formating strings)
+#imie = 'Jan'
+#nazwisko = 'Kowalski'
+#wiadomosc = imie + ' [' + nazwisko + '] ' + 'jest programista'
+#wiado = f'{imie} [{nazwisko}] jest programista'
+# print(wiadomosc)
+# print(wiado)
+#kurs = 'python dla poczatkujacych'
+#print(kurs.replace('p', 'n'))
+#print('python' in kurs)
+# len()
+# print(kurs.upper())
+# print(kurs.lower())
+# print(kurs.title())
+# print(kurs.find('p'))
+# print(kurs.replace())
+
+# operacje arytmetyczne (Arithmetical Operations)
+# print(10 + 3)  # addition
+# print(10 - 3)  # subtraction
+# print(10 * 3)  # multiplication
+# print(10 / 3)  # division1
+# print(10 // 3)  # division2 only intiger
+# print(10 % 3)  # modulis returns remainder of the devision
+# print(10 ** 3)  # power
+
+# funkcje matematyczne (math function)
+#import math
+# print(math.ceil(2.9))
+# print(math.floor(2.9))
+#x = 2.9
+# print(round(x))  # =3
+# print(abs(-2.9))  # = 2.9
