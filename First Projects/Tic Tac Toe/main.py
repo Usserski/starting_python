c18541e23cfcdc7f8d9b78c0d9f797723e95177f
Usserski <114247414+Usserski@users.noreply.github.com>
2023-08-29 08:30:25 +0200

    Update main.py

--- First Projects/Tic Tac Toe/main.py
+++ First Projects/Tic Tac Toe/main.py
@@ -33,12 +33,50 @@ class Board:
 
     def __init__(self):
         self.squares = np.zeros((ROWS, COLS))
+        self.empty_sqrs = self.squares
+        self.marked_sqrs = 0
+
+    def final_state(self):
+
+        # vertical
+        for col in range(COLS):
+            if self.squares[0][col] == self.squares[1][col] == self.squares[2][col] != 0:
+                return self.squares[0][col]
+
+        # horizontal
+        for row in range(ROWS):
+            if self.squares[row][0] == self.squares[row][1] == self.squares[row][2] != 0:
+                return self.square[row][0]
+
+        # desc diagonal
+            if self.squares[0][0] == self.squares[1][1] == self.squares[2][2] != 0:
+                return self.squares[0][0]
+        # asc diagonal
+            if self.squares[2][0] == self.squares[1][1] == self.squares[0][2] != 0:
+                return self.squares[2][0]
+
+        return 0
 
     def mark_sqr(self, row, col, player):
         self.squares[row][col] = player
+        self.marked_sqrs += 1
 
-    def empty_sqr(self, row, col):
+    def empty_sqrs(self, row, col):
         return self.squares[row][col] == 0
+
+    def get_empty_sqrs(self):
+        empty_sqrs = []
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.empty_sqrs(row, col):
+                    empty_sqrs.append((row, col))
+        return empty_sqrs
+
+    def isfull(self):
+        return self.marked_sqrs == 9
+
+    def isempty(self):
+        return self.marked_sqrs == 0
 
 
 class Game:
@@ -86,12 +124,15 @@ class Game:
     def next_turn(self):
         self.player = self.player % 2 + 1
 
+    def check_win(self, row, col):
+
 
 def main():
     game = Game()
     board = game.board
+    running = True
 
-    while True:
+    while running:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -105,7 +146,7 @@ def main():
                     board.mark_sqr(row, col, game.player)
                     game.draw_fig(row, col)
                     game.next_turn()
-                    print(board.squares)
+                    game.check_win()
 
         pygame.display.update()
 
