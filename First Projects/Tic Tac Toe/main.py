6705e6cf85378383470f97562046d5ee46c71796
Usserski <114247414+Usserski@users.noreply.github.com>
2023-09-07 18:02:27 +0200

    Update main.py

--- First Projects/Tic Tac Toe/main.py
+++ First Projects/Tic Tac Toe/main.py
@@ -1,7 +1,8 @@
 import pygame
+import copy
 import pygame.font
 import numpy as np
-
+import random
 pygame.init()
 
 WIDTH, HEIGHT = 900, 900
@@ -46,7 +47,7 @@ class Board:
         # horizontal
         for row in range(ROWS):
             if self.squares[row][0] == self.squares[row][1] == self.squares[row][2] != 0:
-                return self.square[row][0]
+                return self.squares[row][0]
 
         # desc diagonal
             if self.squares[0][0] == self.squares[1][1] == self.squares[2][2] != 0:
@@ -68,7 +69,7 @@ class Board:
         empty_sqrs = []
         for row in range(ROWS):
             for col in range(COLS):
-                if self.empty_sqrs(row, col):
+                if self.empty_sqr(row, col):
                     empty_sqrs.append((row, col))
         return empty_sqrs
 
@@ -79,11 +80,95 @@ class Board:
         return self.marked_sqrs == 0
 
 
+class AI:
+
+    def __init__(self, level=1, player=2):
+        self.level = level
+        self.player = player
+
+    def rnd(self, board):
+        empty_sqrs = board.get_empty_sqrs()
+        idx = random.randrange(0, len(empty_sqrs))
+
+        return empty_sqrs[idx]  # row col
+
+    def minimax(self, board, maximizing):
+
+        # terminal case
+        case = board.final_state()
+
+        # player 1 wins
+        if case == 1:
+            return 1, None  # eval move
+
+        # player 2 wins
+        if case == 2:
+            return -1, None
+
+        # draw
+        elif board.isfull():
+            return 0, None
+
+        if maximizing:
+            max_eval = -100
+            best_move = None
+            empty_sqrs = board.get_empty_sqrs()
+
+            for (row, col) in empty_sqrs:
+                temp_board = copy.deepcopy(board)
+                temp_board.mark_sqr(row, col, 1)
+                eval = self.minimax(temp_board, False)[0]
+                if eval > max_eval:
+                    max_eval = eval
+                    best_move = (row, col)
+
+            return max_eval, best_move
+
+        elif not maximizing:
+            min_eval = 100
+            best_move = None
+            empty_sqrs = board.get_empty_sqrs()
+
+            for (row, col) in empty_sqrs:
+                temp_board = copy.deepcopy(board)
+                temp_board.mark_sqr(row, col, self.player)
+                eval = self.minimax(temp_board, True)[0]
+                if eval < min_eval:
+                    min_eval = eval
+                    best_move = (row, col)
+
+            return min_eval, best_move
+
+    def eval(self, main_board):
+        if self.level == 0:
+            # random choice
+            eval = 'random'
+            move = self.rnd(main_board)
+
+        else:
+            eval, move = self.minimax(main_board, False)
+
+        print(
+            f'AI has chosen to mark the square in pos {move} woth an eval of {eval}')
+
+        return move
+
+
 class Game:
     def __init__(self):
+
         self.board = Board()
+        self.ai = AI()
         self.player = 1
+        self.gamemode = 'ai'  # pvp or ai
+        self.running = True
         self.do_lines()
+
+    def make_move(self, row, col):
+
+        self.board.mark_sqr(row, col, self.player)
+        self.draw_fig(row, col)
+        self.next_turn()
 
     def do_lines(self):
         # pionowe
@@ -92,7 +177,6 @@ class Game:
 
         pygame.draw.line(window, LINE_COLOR, (WIDTH - SQSIZE, 0),
                          (WIDTH-SQSIZE, HEIGHT), LINE_WIDTH)
-
 
         # poziome
         pygame.draw.line(window, LINE_COLOR, (0, SQSIZE),
@@ -124,18 +208,18 @@ class Game:
     def next_turn(self):
         self.player = self.player % 2 + 1
 
-    
-
 
 def main():
     game = Game()
     board = game.board
+    ai = game.ai
     running = True
 
     while running:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                running = False
                 pygame.quit()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -143,13 +227,19 @@ def main():
                 row = pos[1] // SQSIZE
                 col = pos[0] // SQSIZE
                 if board.empty_sqr(row, col):
-                    board.mark_sqr(row, col, game.player)
-                    game.draw_fig(row, col)
-                    game.next_turn()
-                    
+                    game.make_move(row, col)
+
+        if game.gamemode == 'ai' and game.player == ai.player:
+            pygame.display.update()
+
+            # ai methods
+
+            row, col = ai.eval(board)
+            game.make_move(row, col)
 
         pygame.display.update()
 
+    running = False
 
-if __name__ == "__main__":
-    main()
+
+main()
