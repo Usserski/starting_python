ffefc65e24b379f8ba9b3e620ea02b433bd506b1
Usserski <usserusserski@gmail.com>
2024-06-10 19:02:16 +0200

    Update hangman_noob.py

--- First Projects/Hangman/hangman_noob.py
+++ First Projects/Hangman/hangman_noob.py
@@ -1,25 +1,36 @@
 import random
 
 def game():
-    words = ['tomek' ,'robak' , 'marchewka' , 'lato' ]
+    words = ['thomas' ,'bug' , 'carrot' , 'summer' ]
     word = random.choice(words)
     letters = ['_'] * len(word)
     lives = 10
     
     while lives > 0:
-        quess = input('Podaj literke: ')
         print(word)
         print(letters)
-        if quess in  letters:
-            for i in  letters:
-                if quess == letters[i]:
-                    letters[i] = quess
-                
-        else:
+        quess = input('Type letter: ')
+        if quess in  word:
+            index_to_change = [i for i , character in enumerate(word) if character == quess]
+            for i in index_to_change:
+                letters[i]=quess
+        else: 
             lives -=1
-            print('Pudło')    
+            print("Miss! Try one more time ")
+            print( "lives count:  " + str(lives))
+            
+        if '_'  not in letters :
+            print('You WON!')
+            return
+            
+        if lives == 0:
+            print('You LOSE !')
+                
+                
+                
+           
             
                   
-    print('Przegrałes')
-    exit()
+ 
+  
 game()
