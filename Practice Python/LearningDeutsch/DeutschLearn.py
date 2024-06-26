a64dba41c9151a0af44056b21aef975c20082f65
Usserski <usserusserski@gmail.com>
2024-06-26 17:41:18 +0200

    Update DeutschLearn.py

--- Practice Python/LearningDeutsch/DeutschLearn.py
+++ Practice Python/LearningDeutsch/DeutschLearn.py
@@ -1,7 +1,7 @@
 import random
 def main():
     
-    with open('Baza niemiecki' , 'r') as file:
+    with open('Baza niemiecki.txt' , 'r') as file:
         Baza = file.read()
     for i in range(Baza):
         print(Baza)
