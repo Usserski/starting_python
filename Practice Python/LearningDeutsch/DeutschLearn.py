ddc4ba78340452390224b7d3f985ef2d4b1cf153
Usserski <usserusserski@gmail.com>
2024-06-26 18:18:29 +0200

    Update DeutschLearn.py

--- Practice Python/LearningDeutsch/DeutschLearn.py
+++ Practice Python/LearningDeutsch/DeutschLearn.py
@@ -1,10 +1,27 @@
 import random
+def readIO():
+    with open('Baza.txt' , 'r' ) as file:
+        Polnisch_words = [ ]
+        Deutsch_words = [ ]
+        for line in file:
+        
+         parts = line.strip().split('-')
+        
+         if len(parts)== 2:
+            
+                Polnisch_words.append(parts[0])
+                Deutsch_words.append(parts[1])
+        else:
+                print(f"Error: Not correct format line in file")
+
+    return (Polnisch_words , Deutsch_words)
+
+
 def main():
     
-    with open('Baza niemiecki.txt' , 'r') as file:
-        Baza = file.read()
-    for i in range(Baza):
-        print(Baza)
+    readIO()
+    
+     
     
 
 main()
