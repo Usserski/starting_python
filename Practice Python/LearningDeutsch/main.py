6c92a009abb4d102a96409b42c2fdfcbef35fbb7
Usserski <usserusserski@gmail.com>
2024-09-23 13:52:12 +0200

    Update main.py

--- Practice Python/LearningDeutsch/main.py
+++ Practice Python/LearningDeutsch/main.py
@@ -1,7 +1,8 @@
 import pygame 
 import sys
+import pdfplumber
+import random
 
-from DeutschLearn import readIO
 
 WIDTH = 1000
 HEIGHT = 800
@@ -44,20 +45,38 @@ class Button():
 class Game():
     def __init__(self):
         pygame.init()
-        
         pygame.display.set_caption("Learning Deutsch")
         self.screen_game = pygame.display.set_mode((WIDTH,HEIGHT))
         self.clock = pygame.time.Clock()
         self.play_button = Button(400 , 400 , 'Play')
         self.banner = Banner("Witaj w grze. Nacisnij 'Play' aby sprawdzić się ze swoim niemieckim ")  
-     
-
-
-
+        self.words_data = self.col_data()
+        self.random_word = None
+        
+    def col_data(self):
+        words_data = []
+        with pdfplumber.open('D:\OneDrive\Dokumenty\GitHub\starting_python\Practice Python\LearningDeutsch\baza_slow.pdf') as file:
+            for page in file.pages:
+                tables = page.extract_table()
+                for table in tables:
+                    for row in table:
+                        words_data.append(row)
+                         
+    
+        return words_data     
+            
+        
+    def randomize_words(self): 
+        if self.words_data:
+            self.random_word = random.choice(self.words_data)
+            
+        
     def run(self):
         while True:
             self.screen_game.fill('white')
             hover = self.play_button.button_rect.collidepoint(pygame.mouse.get_pos())
+            self.banner.draw(self.screen_game)
+            self.play_button.draw(self.screen_game , hover)
            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -65,14 +84,15 @@ class Game():
                    sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
                    if self.play_button.button_rect.collidepoint(event.pos):
-                      print("Button clicked!")
-            
-            self.banner.draw(self.screen_game)
-            self.play_button.draw(self.screen_game , hover)
+                        self.randomize_words()  # Wybierz losowe słowo
+                        print(f"Wylosowane słowo: {self.random_word}")
+                      
+                      
             pygame.display.update()
             self.clock.tick(60)
             
 
 if __name__ == "__main__":
-    Game().run()
+    game = Game()
+    game.run()
 
